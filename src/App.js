f2a2d847de999ee2f7d3480418abb62f83f55694
Santiago <satovarr@unal.edu.co>
2022-03-01 01:08:14 -0500

    tabs added, please add react-table

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import './App.css';
 import { useEffect, useState } from 'react';
 import axios from 'axios';
+import "./components/FontawsomeIcons";
+import SimpleTabs from "./components/SimpleTab/index.js";
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
 
 function App() {
 
@@ -23,48 +27,69 @@ function App() {
   }, [])
 
   return (
+
+    <Router>
+
     <div className="App">
 
-      <div className="containerInput">
-        <input value={busqueda} placeholder="Busqueda por nombre" />
-        <button className="success-btn">
-          
-        </button>
+
+      <div className='content'>
+          <Switch>
+            <Route exact path='/'>
+              
+            </Route>
+            <Route path='/Tab'>
+              <SimpleTabs/>
+            </Route>
+          </Switch>    
       </div>
 
-      <div className="table-container">
-        <table className="table">
-          <thead>
-            <tr>
-              <th>ID</th>
-              <th>Nombre</th>
-              <th>Telefono</th>
-              <th>Nombre del usuario</th>
-              <th>Correo</th>
-              <th>Sitio Web</th>
-              <th>Ciudad</th>
-              <th>Empresa</th>
-            </tr>
-          </thead>
-        </table>
-
-        <tbody>
-          {usuarios && usuarios.map((usuario) => (
-            <tr key={usuario.id}>
-              <td>{usuario.id}</td>
-              <td>{usuario.name}</td>
-              <td>{usuario.phone}</td>
-              <td>{usuario.username}</td>
-              <td>{usuario.email}</td>
-              <td>{usuario.website}</td>
-              <td>{usuario.address.city}</td>
-              <td>{usuario.company.name}</td>
-            </tr>
-          ))}
-        </tbody>
+      
       </div>
-    </div>
+    </Router>
   );
 }
 
 export default App;
+
+/**
+ * 
+ * <div className="containerInput">
+                <input value={busqueda} placeholder="Busqueda por nombre" />
+                <button className="success-btn">
+
+                </button>
+              </div>
+
+              <div className="table-container">
+                <table className="table">
+                  <thead>
+                    <tr>
+                      <th>ID</th>
+                      <th>Nombre</th>
+                      <th>Telefono</th>
+                      <th>Nombre del usuario</th>
+                      <th>Correo</th>
+                      <th>Sitio Web</th>
+                      <th>Ciudad</th>
+                      <th>Empresa</th>
+                    </tr>
+                  </thead>
+                </table>
+
+                <tbody>
+                  {usuarios && usuarios.map((usuario) => (
+                    <tr key={usuario.id}>
+                      <td>{usuario.id}</td>
+                      <td>{usuario.name}</td>
+                      <td>{usuario.phone}</td>
+                      <td>{usuario.username}</td>
+                      <td>{usuario.email}</td>
+                      <td>{usuario.website}</td>
+                      <td>{usuario.address.city}</td>
+                      <td>{usuario.company.name}</td>
+                    </tr>
+                  ))}
+                </tbody>
+              </div>
+ */
